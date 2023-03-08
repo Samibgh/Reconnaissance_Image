0939b2fba7c5e715e411407d8f6d3b09962c6717
Louison Papazian <louison.papazian@outlook.fr>
2023-03-08 17:06:36 +0100

    update

--- Louison.py
+++ Louison.py
@@ -1,108 +1,42 @@
-import face_recognition
-import os, sys
 import cv2
 import numpy as np
-import math
+import face_recognition as fr
 
-# Helper
-def face_confidence(face_distance, face_match_threshold=0.6):
-    range = (1.0 - face_match_threshold)
-    linear_val = (1.0 - face_distance) / (range * 2.0)
+video_capture = cv2.VideoCapture(0)
+image = fr.load_image_file("faces/Louison Papazian.jpeg")
+image_face_encoding = fr.face_encodings(image)[0]
+known_face_encodings = [image_face_encoding]
+known_face_names = ["Louison Papazian"]
 
-    if face_distance > face_match_threshold:
-        return str(round(linear_val * 100, 2)) + '%'
-    else:
-        value = (linear_val + ((1.0 - linear_val) * math.pow((linear_val - 0.5) * 2, 0.2))) * 100
-        return str(round(value, 2)) + '%'
+while True:
 
-class FaceRecognition:
-    face_locations = []
-    face_encodings = []
-    face_names = []
-    known_face_encodings = []
-    known_face_names = []
-    process_current_frame = True
+    ret, frame = video_capture.read()
 
-    def __init__(self):
-        self.encode_faces()
+    rgb_frame = frame[:, :, ::-1]
 
-    def encode_faces(self):
-        for image in os.listdir('faces'):
-            face_image = face_recognition.load_image_file(f"faces/{image}")
-            if len(face_recognition.face_encodings(face_image)) > 0:
-                face_encoding = face_recognition.face_encodings(face_image)[0]
-            else:
-                print(f"No face found in {image}")
-                continue
-            self.known_face_encodings.append(face_encoding)
-            self.known_face_names.append(image)
-        print(self.known_face_names)
+    fc_locations = fr.face_locations(rgb_frame)
+    fc_encodings = fr.face_encodings(rgb_frame, fc_locations)
 
-    def run_recognition(self):
-        video_capture = cv2.VideoCapture(0)
+    for (top, right, bottom, left), fc_encoding in zip(fc_locations, fc_encodings):
+        matches = fr.compare_faces(known_face_encodings, fc_encoding)
+        name = "Unknown"
 
-        if not video_capture.isOpened():
-            sys.exit('Video source not found...')
+        fc_distances = fr.face_distance(known_face_encodings, fc_encoding)
 
-        while True:
-            ret, frame = video_capture.read()
+        match_index = np.argmin(fc_distances)
 
-            # Only process every other frame of video to save time
-            if self.process_current_frame:
-                # Resize frame of video to 1/4 size for faster face recognition processing
-                small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        if matches[match_index]:
+            name = known_face_names[match_index]
 
-                # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-                rgb_small_frame = small_frame[:, :, ::-1]
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
-                # Find all the faces and face encodings in the current frame of video
-                self.face_locations = face_recognition.face_locations(rgb_small_frame)
-                self.face_encodings = face_recognition.face_encodings(rgb_small_frame, self.face_locations)
+    cv2.imshow('Simplilearn face', frame)
 
-                self.face_names = []
-                for face_encoding in self.face_encodings:
-                    # See if the face is a match for the known face(s)
-                    matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
-                    name = "Unknown"
-                    confidence = '???'
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-                    # Calculate the shortest distance to face
-                    face_distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
-
-                    best_match_index = np.argmin(face_distances)
-                    if matches[best_match_index]:
-                        name = self.known_face_names[best_match_index]
-                        confidence = face_confidence(face_distances[best_match_index])
-
-                    self.face_names.append(f'{name} ({confidence})')
-
-            self.process_current_frame = not self.process_current_frame
-
-            # Display the results
-            for (top, right, bottom, left), name in zip(self.face_locations, self.face_names):
-                # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-                top *= 4
-                right *= 4
-                bottom *= 4
-                left *= 4
-
-                # Create the frame with the name
-                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-                cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-                cv2.putText(frame, name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 255, 255), 1)
-
-            # Display the resulting image
-            cv2.imshow('Face Recognition', frame)
-
-            # Hit 'q' on the keyboard to quit!
-            if cv2.waitKey(1) == ord('q'):
-                break
-
-        # Release handle to the webcam
-        video_capture.release()
-        cv2.destroyAllWindows()
-
-
-if __name__ == '__main__':
-    fr = FaceRecognition()
-    fr.run_recognition()
+video_capture.release()
+cv2.destroyAllWindows()
