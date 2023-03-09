26c90f159fbc1a42a6f8c36867b508cb928b534c
Samibgh <78345903+Samibgh@users.noreply.github.com>
2023-03-09 18:00:46 +0100

    change

--- main.py
+++ main.py
@@ -4,7 +4,16 @@ import os, sys
 import numpy as np
 import math
 import pickle
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
+from keras.models import load_model
+#from keras.preprocessing.image import img_to_array
+from keras_preprocessing.image import img_to_array
+
+face_classifier = cv2.CascadeClassifier('models/haarcascade_frontalface_default.xml')
+classifier =load_model('models/Emotion_detector.h5')
+
+class_labels = ['Angry','Happy','Neutral','Sad','Surprise']
 MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
 AGE_INTERVALS = ['(0, 3)', '(4, 6)','(7, 12)', '(13,15)','(16, 20)', '(21, 25)', '(26,29)','(30, 35)', '(36,39)','(40, 45)','(46,49)','(50, 55)','(56, 59)','(60, 65)','(66, 70)','(71, 75)','(76, 80)','(81, 85)','(86, 90)','(91, 95)','(96, 100)']
 
@@ -84,7 +93,10 @@ class FaceRecognition:
                 # Find all the faces and face encodings in the current frame of video
                 self.face_locations = face_recognition.face_locations(rgb_small_frame)
                 self.face_encodings = face_recognition.face_encodings(rgb_small_frame, self.face_locations)
+                gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+                faces = face_classifier.detectMultiScale(gray,1.2,5)
 
+                
                 self.face_names = []
                 for face_encoding in self.face_encodings:
                     # See if the face is a match for the known face(s)
@@ -104,6 +116,30 @@ class FaceRecognition:
                     self.face_names.append(f'{name} ({confidence})')
 
 
+                list_emotions =[]
+                list_emotions_positions =[]
+                for (x,y,w,h) in faces:
+                    #cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+                    roi_gray = gray[y:y+h,x:x+w]
+                    try:
+                        roi_gray = cv2.resize(roi_gray,(48,48),interpolation=cv2.INTER_AREA)
+                    except:
+                        continue
+            
+            
+                    if np.sum([roi_gray])!=0:
+                        roi = roi_gray.astype('float')/255.0
+                        roi = img_to_array(roi)
+                        roi = np.expand_dims(roi,axis=0)
+                        
+                    # make a prediction on the ROI, then lookup the class
+                    preds = classifier.predict(roi)[0]
+                    label=class_labels[preds.argmax()]
+                    label_position = (x,y)
+                    
+                    list_emotions.append(label)
+                    list_emotions_positions.append(label_position)
+                 
             self.process_current_frame = not self.process_current_frame
             face_img = frame[0:20,0:20]
             blob = cv2.dnn.blobFromImage(face_img, 1.0,size=(227, 227),
@@ -140,10 +176,12 @@ class FaceRecognition:
                 right *= 4
                 bottom *= 4
                 left *= 4
+
                 # Create the frame with the name
                 font = cv2.FONT_HERSHEY_DUPLEX
                 cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
                 cv2.putText(frame, name, (left - 100, bottom - 180), font, 0.8, (255, 255, 255), 1)
+                cv2.putText(frame, label, (left + 6, bottom + 100), font, 1, (0,240,255), 1)
                 cv2.putText(frame, label_age, (left + 6, bottom + 30), font, 1.0, (255, 255, 0), 1)
                 cv2.putText(frame, gender, (left + 6, bottom + 70), font, 1.0, (0, 0, 255), 1)
 
