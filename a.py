1ad6ac37ca59499c47c7b94a5a9f7342a74416d0
Louison Papazian <louison.papazian@outlook.fr>
2023-03-09 23:30:53 +0100

    pu

--- a.py
+++ a.py
@@ -2,6 +2,7 @@ import main_thread
 import streamlit as st
 import cv2
 import numpy as np
+import speech_recognition as sr
 
 # Streamlit initialization
 st.title("Face Recognition")
@@ -28,5 +29,30 @@ def main():
     cap.release()
     cv2.destroyAllWindows()
 
-if __name__ == "__main__":
-    main()
+st.title("Voice-controlled Webcam")
+st.sidebar.write("Barre des messages : ")
+st.session_state.listen_for_commands = False
+r = sr.Recognizer()
+run_button = st.checkbox("Activate Voice Control")
+
+
+if run_button:
+    with sr.Microphone() as source:
+        st.write("Dites 'tarte' pour démarrer la webcam")
+        r.adjust_for_ambient_noise(source)
+        while True:
+            audio = r.listen(source, phrase_time_limit=5)
+            try:
+                text = r.recognize_google(audio, language='fr-FR')
+                if text.lower() == "tarte":
+                    st.session_state.run = True
+                    main()
+                else:
+                    st.sidebar.warning(f"Je n'ai pas reconnu la commande: {text}")
+            except sr.UnknownValueError:
+                st.sidebar.warning("Je n'ai pas entendu")
+            except sr.RequestError as e:
+                st.sidebar.error(f"Erreur; {e}")
+
+#if __name__ == "__main__":
+ #   main()
